266317c1007440673dc4448890199ded6a6e431f
Unknown <ashley.warner01@gmail.com>
2017-09-10 12:06:26 -0500

    JS Should Function

--- Adoptions/adoptions.js
+++ Adoptions/adoptions.js
@@ -1,4 +1,17 @@
-function imageClickChamp() {
+function imageClick() {
     "use strict";
-    document.getElementsByClassName("modalClass").style.display = 'block';
+    document.getElementById("modalId").style.display = 'block';
 }
+
+function imageClose() {
+    "use strict";
+    document.getElementById("modalId").style.display = 'none';
+}
+
+window.onclick = function (event) {
+    "use strict";
+    if (event.target === document.getElementById("modalId")) {
+        document.getElementById("modalId").style.display = 'none';
+    }
+};
+
