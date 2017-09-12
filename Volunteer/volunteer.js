a9416cfd03ac0ee20ecb787e7826ef8933a76425
James <jmcadoo89@gmail.com>
2017-09-11 21:06:30 -0500

    Added a Volunteer form to match the adoption form / messed around with the nav bar style (it's commented out currently - stil a WIP) / added ashley's js for the form submit button

---
+++ Volunteer/volunteer.js
@@ -0,0 +1,5 @@
+/*Submit Form*/
+function formSubmit() {
+    "use strict";
+    window.alert("Thank you! Your form has been submitted and an Adoptions Representative will contact you shortly.");
+}
