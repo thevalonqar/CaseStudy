a2dfb788f0d73a908edacd16a92b239486e64c7a
James <jmcadoo89@gmail.com>
2017-09-15 20:50:16 -0500

    added footer w/ css and js from index.html to volunteer.html

--- Volunteer/volunteer.js
+++ Volunteer/volunteer.js
@@ -3,3 +3,9 @@ function formSubmit() {
     "use strict";
     window.alert("Thank you! Your form has been submitted and an Volunteer Representative will contact you shortly.");
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
