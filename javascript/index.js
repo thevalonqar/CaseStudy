8d8cf4028d271dab609e59d48ab5ea4c72458f0c
Sara <snsimpson1202@gmail.com>
2017-09-17 10:59:33 -0500

    created array

--- javascript/index.js
+++ javascript/index.js
@@ -15,3 +15,29 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// array for testimonials    
+    var testimonials = [
+        ["This shelter is amazing!", "Mary"],
+        ["Animal Shelter gave me my best friend!", "Tom"],
+        ["Animal Shelter helps so many dogs and pups in need.", "Molly"],
+        ["Volunteering at Animal Shelter is the best", "Bill"]
+        ["I adopted all of my dogs from here!", "Sue"]
+]
+
+function displayTestimonials() {
+    "use strict";
+    var testimonies = "";
+    
+    for(var i =0; i < testimonials.length; i++){
+        testimonies += "<p>&ldquo;" + testimonials[i][0] + "&rdquo; &mdash; <em>" + testimonials[i][1] + "</em></p>";
+    }
+    document.getElementById("testimonial").innerHTML = testimonies;
+};
+
+// displays testimonies on load, backwards compatible
+if(window.addEventListener){
+   window.addEventListener("load", displayTestimonials, false);
+} else if (window.attachEvent){
+   window.attachEvent("onload", displayTestimonials);
+} 
