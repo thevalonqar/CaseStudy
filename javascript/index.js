4414984eafcbd1c4785793d3a0faaf7de62dbf6b
Unknown <ashley.warner01@gmail.com>
2017-09-17 20:09:12 -0500

    Added button and rotation to testimonial

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,9 @@
 // back to top button 
 // When the user scrolls down 100px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {"use strict"; scrollFunction(); };
 
 function scrollFunction() {
+    "use strict";
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.getElementById("topButton").style.display = "block";
     } else {
@@ -12,6 +13,7 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
+    "use strict";
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
@@ -30,10 +32,16 @@ function displayTestimonials() {
     var testimonies = "";
     
     for(var i =0; i < testimonials.length; i++){
-        testimonies += "<p>&ldquo;" + testimonials[i][0] + "&rdquo; &mdash; <em>" + testimonials[i][1] + "</em></p>";
+        testimonies = "<p  id='quoteP'>&ldquo;" + testimonials[i][0] + "&rdquo; &mdash; <em>" + testimonials[i][1] + "</em></p>";
     }
     document.getElementById("testimonial").innerHTML = testimonies;
-};
+}
+
+function seeMore() {
+    "use strict";
+    var r = Math.floor(Math.random() * 5) + 0;
+    document.getElementById("quoteP").innerHTML = "<p>&ldquo;" + testimonials[r][0] + "&rdquo; &mdash; <em>" + testimonials[r][1] + "</em></p>";
+}
 
 // displays testimonies on load, backwards compatible
 if(window.addEventListener){
